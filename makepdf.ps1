51f1acf7b9f1a56abd0c96886e60f73a9a00a691
John Stegink <74827523+johnstegink@users.noreply.github.com>
2025-11-08 15:22:54 +0100

    Hessian matrices

--- makepdf.ps1
+++ makepdf.ps1
@@ -1,1 +1,7 @@
-pdflatex -output-directory="pdf" -jobname=project-EvertJan-Karman--John-Stegink project-EvertJan-Karman--John-Stegink.tex
+@misc{calcworkshopcom2022,
+   author = {calcworkshop.com},
+   journal = {https://calcworkshop.com/partial-derivatives/partial-derivative/},
+   month = {1},
+   title = {Partial Derivative Fully Explained w/ Step-by-Step Examples},
+   year = {2022}
+}
