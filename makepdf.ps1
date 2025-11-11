38bd3b46faf5852f4f055a394e4fc680165e2f09
Evert Jan Karman <89303498+evert823@users.noreply.github.com>
2025-11-11 14:17:12 +0100

    only one pdf version

--- makepdf.ps1
+++ makepdf.ps1
@@ -1,1 +1,2 @@
 pdflatex -output-directory="pdf" -jobname=project-EvertJan-Karman--John-Stegink project-EvertJan-Karman--John-Stegink.tex
+Copy-Item -Path .\pdf\project-EvertJan-Karman--John-Stegink.pdf -Destination .\ -Force
